e90ba7a95195a12faa8edfc3d7ddd98c2f9610cd
Samuel Dionne-Riel <samuel@dionne-riel.com>
2024-07-29 17:13:33 -0400

    Start parsing

---
+++ lib/parser.rb
@@ -0,0 +1,164 @@
+require_relative "lexer"
+
+OPCODES_BY_BYTE = {
+  0x00 => "BRK",
+  0x01 => "INC",
+  0x02 => "POP",
+  0x03 => "NIP",
+  0x04 => "SWP",
+  0x05 => "ROT",
+  0x06 => "DUP",
+  0x07 => "OVR",
+  0x08 => "EQU",
+  0x09 => "NEQ",
+  0x0a => "GTH",
+  0x0b => "LTH",
+  0x0c => "JMP",
+  0x0d => "JCN",
+  0x0e => "JSR",
+  0x0f => "STH",
+
+  0x10 => "LDZ",
+  0x11 => "STZ",
+  0x12 => "LDR",
+  0x13 => "STR",
+  0x14 => "LDA",
+  0x15 => "STA",
+  0x16 => "DEI",
+  0x17 => "DEO",
+  0x18 => "ADD",
+  0x19 => "SUB",
+  0x1a => "MUL",
+  0x1b => "DIV",
+  0x1c => "AND",
+  0x1d => "ORA",
+  0x1e => "EOR",
+  0x1f => "SFT",
+
+  0x20 => "JCI",
+  0x40 => "JMI",
+  0x60 => "JSI",
+
+  0x80 => "LIT",
+  0xa0 => "LIT2",
+  0xc0 => "LITr",
+  0xe0 => "LIT2r",
+}
+
+(0x01..0x1f).each do |num|
+  OPCODES_BY_BYTE[ num | 0x20 ] = OPCODES_BY_BYTE[num] + "2"
+  OPCODES_BY_BYTE[ num | 0x40 ] = OPCODES_BY_BYTE[num] + "r"
+  OPCODES_BY_BYTE[ num | 0x60 ] = OPCODES_BY_BYTE[num] + "2r"
+  OPCODES_BY_BYTE[ num | 0x80 ] = OPCODES_BY_BYTE[num] + "k"
+  OPCODES_BY_BYTE[ num | 0xa0 ] = OPCODES_BY_BYTE[num] + "2r"
+  OPCODES_BY_BYTE[ num | 0xc0 ] = OPCODES_BY_BYTE[num] + "kr"
+  OPCODES_BY_BYTE[ num | 0xe0 ] = OPCODES_BY_BYTE[num] + "2kr"
+end
+
+OPCODES_BY_MNEMONIC = OPCODES_BY_BYTE.map do |k, v|
+  [v, k]
+end.sort.to_h
+
+class Parser
+  attr_reader :tokens
+  attr_reader :output
+  attr_reader :labels
+
+  def initialize(tokens)
+    @tokens = tokens.select { |token| !token.transparent? }
+    @output = []
+    # Start at 0x0100, where ROM starts (XXX: verify it's the correct behaviour)
+    @position = 0x0100
+    @labels = {}
+  end
+
+  def parse!()
+    # First coalesce the tokens into their final shape... with palceholders...
+    tokens.each do |token|
+      case token
+      when Lexer::PaddingRelative
+        @position += token.value
+      when Lexer::PaddingAbsolute
+        @position = token.value
+      when Lexer::Label
+        # The labels were checked for uniqueness when lexing...
+        labels[token.str] = @position
+      when Lexer::Literal
+        if token.value[:length] == 1
+          add_op("LIT")
+        else
+          add_op("LIT2")
+        end
+        add_output(Output.new(token.value[:value], length: token.value[:length], position: @position))
+      when Lexer::ByteOrShort
+        add_output(Output.new(token.value[:value], length: token.value[:length], position: @position))
+      when Lexer::RawAscii
+        add_output(Output.new(token.value, length: token.value.length, position: @position))
+      when Lexer::Opcode
+        add_op(token.str)
+      when Lexer::LabelRef
+        output << Placeholder.new(token.str, token.ref_type, position: @position)
+      else
+        raise "Unexpected token #{token.class.name.inspect}"
+      end
+    end
+
+    # Then pry out the placeholder for actual values
+    output.each do |value|
+      next unless value.is_a?(Placeholder)
+      value.set_ref(@labels[value.label])
+    end
+
+    output
+  end
+
+  def add_op(name)
+    add_output(Output.new(OPCODES_BY_MNEMONIC[name], position: @position))
+  end
+
+  def add_output(value)
+    output << value
+    @position += value.length
+  end
+
+  class Output
+    # Value as either a number (8 or 16 bit) or a string of bytes.
+    attr_reader :value
+    # Length in bytes of the value
+    attr_reader :length
+    # Position (in RAM)
+    attr_reader :position
+
+    def initialize(value, length: 1, position:)
+      @value = value
+      @length = length
+      @position = position
+    end
+  end
+
+  class Placeholder < Output
+    attr_reader :label
+    attr_reader :type
+
+    def initialize(label, type, position:)
+      @length =
+        if [:relative_8, :zeropage].include?(type)
+          1
+        else
+          2
+        end
+      super(nil, length: @length, position: position)
+      @label = label
+      @type = type
+    end
+
+    def set_ref(address)
+      @value =
+        if [:relative_8, :relative_16].include?(type)
+          address - position
+        else
+          address
+        end
+    end
+  end
+end
--- test.rb
+++ test.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
 
 require_relative "lib/lexer"
+require_relative "lib/parser"
 
 lexer = Lexer.from_file(ARGV.first)
 lexer.parse!
 lexer.preprocess!
 
-pp (lexer.tokens.select { |token| !token.transparent?})
+parser = Parser.new(lexer.tokens)
+parser.parse!
